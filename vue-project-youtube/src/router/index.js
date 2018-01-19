c2267ed6948d504e5a9b3b7ec239d0ac97da5c66
Justin Hynes-Bruell <hynes-bruell.j@husky.neu.edu>
2018-01-18 20:54:25 -0500

    asdasdasd

--- vue-project-youtube/src/router/index.js
+++ vue-project-youtube/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import TriviaQuestion from '@/components/TriviaQuestion'
+import navBar from '@/components/navBar'
 
 Vue.use(Router)
 
